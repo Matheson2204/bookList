b9f877021a85c104d7560327b0705c82aa214a10
Matheson_Walsh <mathesonw@optusnet.com.au>
2021-09-15 15:13:47 +1000

    completed exercise.js

--- exercises.js
+++ exercises.js
@@ -1,6 +1,18 @@
 // Objects
 
 // 1. Create an object in Javascript assigned to a variable named ‘school’ that contains the following properties:
+const school = {
+    name: "Hogwarts",
+    location: "Unknown",
+    established: "the 10th Century",
+
+    displayInfo() {
+        console.log(`My school ${this.name} was established in ${this.established} at ${this.location}`);
+    }
+};
+
+console.log(school.name);
+school.displayInfo();
 // name: "Hogwarts"
 // location: "Unknown"
 // established: "the 10th Century"
@@ -13,6 +25,22 @@
 
 // We will now create a template for making school objects! You may want to refer to Class Syntax: https://www.w3schools.com/js/js_classes.asp ;
 // 1. create a class called CreateSchool
+class CreateSchool {
+    constructor(name, location, established) {
+        this._name = name;
+        this._location = location;
+        this._established = established;
+    }
+    displayInfo() {
+        console.log(`My school ${this._name} was established in ${this._established} at ${this._location}`);
+    }
+}
+const hogwarts = new CreateSchool('hogwarts', 'Unknown', 'the 10th Century' );
+console.log(hogwarts);
+
+hogwarts.displayInfo()
+    
+
 // a) in its constructor method, give it the properties of name, location and established
 // b) add a method called displayInfo that console.logs the properties
 // c) Using this class, create a new instance (object) with the values of the object we made above
