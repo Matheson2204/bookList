f6d8fe7e5aefe3f9e9531d12252c5074521a544c
Lisa Ostman <peolina@live.com.au>
2021-09-15 11:43:35 +1000

    updated instructions of booklist

--- booklist.js
+++ booklist.js
@@ -1,40 +1,43 @@
 // BookManager class
+// We are creating a class that will manage a list of books. Each time we add a book, we want our new book to be added to a list with a unique, incremented id.
+// Let's try to code this down below!
+
 class BookManager {
 // this is a constructor with a paramater currentId set to 0 by default
   constructor(currentId = 0) {
-      // initialize an empty array called books
+      // 1. initialize an empty array called books
     
-      // initialize a currentId set to currentId
+      // 2. initialize a currentId set to currentId
   }
 
-  // function addBook creates an object and pushes it to our books array
+   // 3. function getter should return the value of our books array
+   get books() {
+      
+}
+
+  // function addBook should create an object and push it to our books array
   // it should take in a name, genre and author and by default have an empty string
   addBook() {
-      // create an object for the newBook
+      // 4. Create an object assigned to a variable for the new book using parameters given to addBook
       
-          // your object should consist of properties id, name,genre and author with values added when we call this method
+          // a) your object should consist of properties id, name,genre and author with values added when we call this method
           // don't forget that this may require some parameters to be used as values
-          // id should be unique and increment by 1 every new instance
-          // if no argument is given, the properties should equal an empty string
+          // b) id should be unique and increment by 1 every new instance
+          // c) if no argument is given, the properties should equal an empty string
 
-      // push the newly created object to our books array
-      // hint: best practice would be to access our books array using the get method
-      
-  }
-
-  // function getter should return the value of our books array
-  get books() {
+      // 5. Push the newly created object to our books array
+      // hint: note that best practice would be to access our books array using the get method
       
   }
 
 }
 
-//outside the class, create an instance of BookManager
+// 6. outside the class, create an instance of BookManager
 
-// add two books with the following properties
+// 7. add two books with the following properties to our instance
 // "The Alchemist", "Life", "Paulo Coelho"
 // "Time to Play", "Children's Book", "Bluey"
 
-// console log our books array to check that the new books were added
+// 8. console log our books array to check that the new books were added
 
 
